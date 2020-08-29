daf7bfd80eb581eadff9ae032cca1b41547476cc
David L. Woodruff <DLWoodruff@UCDavis.edu>
2020-08-28 18:10:13 -0700

    make the tee example shorter

--- mpisppy/examples/run_all.py
+++ mpisppy/examples/run_all.py
@@ -87,8 +87,12 @@ do_one("farmer", "farmer_cylinders.py", 4,
        "--default-rho=1 --solver-name={} ".format(solver_name))
 do_one("farmer", "farmer_cylinders.py", 2,
        "6 --bundles-per-rank=2 --max-iterations=50 "
-       "--default-rho=1 --with-tee-rank0-solves "
+       "--default-rho=1 "
        "--solver-name={} --no-fwph --no-lagrangian".format(solver_name))
+do_one("farmer", "farmer_cylinders.py", 3,
+       "3 --bundles-per-rank=0 --max-iterations=1 "
+       "--default-rho=1 --with-tee-rank0-solves "
+       "--solver-name={} --no-fwph".format(solver_name))
 do_one("farmer", "farmer_pysp.py", 1, solver_name)
 do_one("sizes",
        "sizes_cylinders.py",
